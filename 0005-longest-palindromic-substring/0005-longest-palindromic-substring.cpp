ad950cb99afc32bee810aa7f9aaded1d45ba39d8
dev-kartikSpace <kartik200327@gmail.com>
2025-08-10 17:03:02 +0530

    Time: 842 ms (12.34%), Space: 9.4 MB (68.14%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,61 +1,29 @@
-// class Solution {
-// public:
-//     bool isPalindrome(const string& s, int left, int right) {
-//         while (left < right) {
-//             if (s[left] != s[right])
-//                 return false;
-//             ++left;
-//             --right;
-//         }
-//         return true;
-//     }
-//     string longestPalindrome(string s) {
-//         int n = s.size();
-//         if (n < 2) return s;
-
-//         int maxLen = 1;
-//         int startIdx = 0;
-
-//         for (int i = 0; i < n; ++i) {
-//             for (int j = i; j < n; ++j) {
-//                 if (isPalindrome(s, i, j) && (j - i + 1) > maxLen) {
-//                     maxLen = j - i + 1;
-//                     startIdx = i;
-//                 }
-//             }
-//         }
-//         return s.substr(startIdx, maxLen);
-//     }
-    
-// };
 class Solution {
 public:
-string checkPali(string s, int ptr1,int ptr2){
-        
-    while(ptr1>=0 && ptr2<s.size() && s[ptr1]==s[ptr2]){
-                ptr2++;
-                ptr1--;
-            }
-            string temp=s.substr(ptr1+1,ptr2-ptr1-1);
-           
-        return temp;
+    bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right])
+                return false;
+            ++left;
+            --right;
         }
-
+        return true;
+    }
     string longestPalindrome(string s) {
-       string result="";
-        int maxi=0;
-        
-        for(int i=0; i<s.size(); i++){
-          int ptr1=i,ptr2=i;
-          while(s[ptr1]==s[ptr2]){
-            ptr2++;
-          }  
-        string temp=checkPali(s,ptr1,ptr2-1);
-         if(temp.size()>maxi){
-                result=temp;
-                maxi=temp.size();
+        int n = s.size();
+        if (n < 2) return s;
+
+        int maxLen = 1;
+        int startIdx = 0;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                if (isPalindrome(s, i, j) && (j - i + 1) > maxLen) {
+                    maxLen = j - i + 1;
+                    startIdx = i;
+                }
             }
-       }
-       return result;
+        }
+        return s.substr(startIdx, maxLen);
     }
 };
