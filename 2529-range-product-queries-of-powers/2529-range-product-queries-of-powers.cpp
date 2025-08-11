e98802bf9dadb8b3310f8657a72e5a1dfb7326fc
dev-kartikSpace <kartik200327@gmail.com>
2025-08-11 11:08:04 +0530

    Time: 13 ms (79.26%), Space: 147.7 MB (50.53%) - LeetHub

---
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        const int MOD = 1e9 + 7;
+        vector<int> powers;
+        for (int i = 0; i < 31; i++) { 
+            if (n & (1 << i)) {         
+                powers.push_back(1 << i);
+            }
+        }
+
+        vector<int> ans;
+        for (auto& q : queries) {
+            int left = q[0];
+            int right = q[1];
+            long long product = 1; 
+            for (int i = left; i <= right; i++) {
+                product = (product * powers[i]) % MOD;
+            }
+            ans.push_back((int)product);
+        }
+        return ans;
+    }
+};
