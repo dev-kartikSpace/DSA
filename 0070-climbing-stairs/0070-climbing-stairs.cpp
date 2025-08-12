efe0521f3f566f3885e29017c0f02e67e1292821
dev-kartikSpace <kartik200327@gmail.com>
2025-08-12 18:55:02 +0530

    Time: 0 ms (100%), Space: 8.6 MB (36.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n + 1, -1); // memoization array
-        return solve(0, n, dp);
+        vector<int>dp(n + 1 , -1);
+        return solve(0 , n ,  dp);
     }
+    int solve(int i , int n, vector<int> &dp){
+        if(i >n) return 0;
+        if(i == n) return 1;
 
-private:
-    int solve(int i, int n, vector<int> &dp) {
-        if (i > n) return 0;     // overshoot
-        if (i == n) return 1;    // exactly reached top
+        if(dp[i] != -1) return dp[i];
 
-        if (dp[i] != -1) return dp[i]; // already computed
-
-        int left = solve(i + 1, n, dp);
-        int right = solve(i + 2, n, dp);
+        int left = solve(i+1 , n , dp);
+        int right = solve(i + 2,n,dp);
 
         return dp[i] = left + right;
     }
