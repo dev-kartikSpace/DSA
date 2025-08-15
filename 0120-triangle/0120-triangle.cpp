de585f27a184dd98aa9fc6049128a55731abf147
dev-kartikSpace <kartik200327@gmail.com>
2025-08-15 12:34:26 +0530

    Time: 0 ms (100%), Space: 13.5 MB (12.14%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        // Initialize dp array with the last row of the triangle
-        vector<int> dp = triangle.back();
-        
-        // Iterate from the second-to-last row to the top
+        vector<int> dp(triangle.back());
+
         for (int i = triangle.size() - 2; i >= 0; i--) {
             vector<int> new_dp(triangle[i].size());
             for (int j = 0; j < triangle[i].size(); j++) {
-                // For each element, choose the minimum of the two possible paths from the row below
-                new_dp[j] = triangle[i][j] + min(dp[j], dp[j + 1]);
+                new_dp[j] = triangle[i][j] + min(dp[j] , dp[j+1]);
             }
-            dp = new_dp; // Update dp for the next row
+            dp = new_dp;
         }
-        
-        // The top element contains the minimum path sum
         return dp[0];
     }
 };
