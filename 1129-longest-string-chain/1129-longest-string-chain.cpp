07472494c4a24fb663c430d87a8d8791a959175b
dev-kartikSpace <kartik200327@gmail.com>
2025-08-19 02:54:50 +0530

    Time: 47 ms (57.86%), Space: 26 MB (43.7%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,29 +1,30 @@
 class Solution {
-public:
     unordered_set<string> st;
     unordered_map<string,int> dp;
-    int bfs(string s){
+    int bfs(string &s){
         int n = s.size();
-        if (n == 1) {
+        if(n==1){
             return 1;
         }
-        if (dp.count(s))
-            return dp[s];
-        int ans = 1;
-        for (int i = 0; i<n; i++) {
-            string curr = s.substr(0, i) + s.substr(i + 1);
+        if(dp.count(s)) return dp[s];
+        int ans=1;
+        for(int i=0;i<n;i++){
+            string curr = s.substr(0,i) + s.substr(i+1); 
             if(st.count(curr))
-                ans = max(ans,1 + bfs(curr));
+                ans=max(ans, 1 + bfs(curr));
         }
-        return dp[s] = ans;
+        return dp[s]=ans;
     }
+public:
     int longestStrChain(vector<string>& words) {
-        for (auto i : words) {
+        int n = words.size();
+        st.clear();
+        for(auto i: words){
             st.insert(i);
         }
-        int ans = 1;
-        for (auto i : words) {
-            ans = max(ans, bfs(i));
+        int ans=1;
+        for(auto i: words){
+            ans=max(ans, bfs(i));
         }
         return ans;
     }
