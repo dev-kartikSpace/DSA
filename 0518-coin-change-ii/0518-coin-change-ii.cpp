8df8393ea5746b72973acff832a8535fb0403fac
dev-kartikSpace <kartik200327@gmail.com>
2025-08-19 12:17:20 +0530

    Time: 37 ms (12.71%), Space: 28.5 MB (39.71%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int n;
-    int solve(int i, vector<int> &coins, int amount, vector<vector<int>> &dp){
-        if(amount == 0) return 1; 
-        if(i == coins.size() || amount < 0) return 0;
-        if(dp[i][amount] != -1) return dp[i][amount];
-        int take = solve(i, coins, amount - coins[i],dp);
-        int skip = solve(i+1, coins, amount,dp);
-        return dp[i][amount] = take + skip;
+    int dfs(int idx, int amount, vector<int> &coins, vector<vector<int>> &dp) {
+        if(amount == 0) return 1;
+        if(idx == coins.size() || amount < 0) return 0;
+        if(dp[idx][amount] != -1) return dp[idx][amount];
+        int include = dfs(idx, amount - coins[idx], coins, dp);
+        int exclude = dfs(idx + 1, amount, coins, dp);
+        return dp[idx][amount] = include + exclude;
     }
     int change(int amount, vector<int>& coins) {
-        n = coins.size();
-        vector<vector<int>> dp(n , vector<int>(amount + 1, -1));
-        return solve(0, coins, amount, dp);
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        return dfs(0, amount, coins, dp);
     }
 };
