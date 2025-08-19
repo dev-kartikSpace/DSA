98aea8a243400172dfc14c0d7aa81c75ac2d8ff0
dev-kartikSpace <kartik200327@gmail.com>
2025-08-19 12:42:15 +0530

    Time: 9 ms (61.32%), Space: 26.5 MB (22.34%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-int n;
-int solve(int idx, vector<int> &nums, int target, int total){
-    if(idx == nums.size())
-    {
-        return total == target ? 1 : 0;
+    int solve(int idx, vector<int> &nums, int target, int total, vector<vector<int>> &dp, int sum) {
+        if (idx == nums.size()) {
+            return total == target ? 1 : 0;
+        }
+        if (dp[idx][total + sum] != -1)
+            return dp[idx][total + sum];
+        
+        int add = solve(idx + 1, nums, target, total + nums[idx], dp, sum);
+        int minus = solve(idx + 1, nums, target, total - nums[idx], dp, sum);
+
+        return dp[idx][total + sum] = add + minus;
     }
-
-    int add = solve(idx + 1, nums , target, total + nums[idx]);
-    int minus = solve(idx + 1, nums , target, total - nums[idx]);
-
-    return add + minus;
-
-}
     int findTargetSumWays(vector<int>& nums, int target) {
-        n = nums.size();
-       return solve(0, nums, target, 0);
+        int n = nums.size();
+        int sum = 0;
+        for (int x : nums) sum += x;
+        // The possible range of total is [-sum...sum]
+        vector<vector<int>> dp(n + 1, vector<int>(2 * sum + 1, -1));
+        return solve(0, nums, target, 0, dp, sum);
     }
 };
