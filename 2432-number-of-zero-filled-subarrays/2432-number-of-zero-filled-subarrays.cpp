469cccf4f710f4440fe74aba5771c171ed5924ca
dev-kartikSpace <kartik200327@gmail.com>
2025-08-19 11:23:32 +0530

    Time: 0 ms (100%), Space: 111.1 MB (95.09%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -3,16 +3,12 @@ public:
     long long zeroFilledSubarray(vector<int>& nums) {
         long long ans = 0, count = 0;
         for (int num : nums) {
-            if (num == 0)
-                count++;
-            else
-                count = 0;
+            count = (num == 0) ? count + 1 : 0;
             ans += count;
         }
         return ans;
     }
 };
-
 // class Solution {
 // public:
 //     long long zeroFilledSubarray(vector<int>& nums) {
