66b6f5a006d209cdc4caaa4bb1c619832ca2af07
dev-kartikSpace <kartik200327@gmail.com>
2025-08-19 12:16:58 +0530

    Time: 35 ms (17.37%), Space: 28.5 MB (45.89%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,19 +2,12 @@ class Solution {
 public:
     int n;
     int solve(int i, vector<int> &coins, int amount, vector<vector<int>> &dp){
-        if(amount == 0) return 1;
-        if(i == n) return 0;
-
+        if(amount == 0) return 1; 
+        if(i == coins.size() || amount < 0) return 0;
         if(dp[i][amount] != -1) return dp[i][amount];
-
-        if(coins[i] > amount){
-            return solve(i+1, coins, amount , dp);
-        }
-
         int take = solve(i, coins, amount - coins[i],dp);
         int skip = solve(i+1, coins, amount,dp);
-
-        return dp[i][amount] = (take + skip);
+        return dp[i][amount] = take + skip;
     }
     int change(int amount, vector<int>& coins) {
         n = coins.size();
