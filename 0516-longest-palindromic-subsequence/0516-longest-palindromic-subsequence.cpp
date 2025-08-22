a318dceb9d028f7106a2531d2a8cebde024a6919
dev-kartikSpace <kartik200327@gmail.com>
2025-08-22 11:40:37 +0530

    Time: 50 ms (82.07%), Space: 75.8 MB (74%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,51 +1,25 @@
-// class Solution {
-// public:
-//     vector<vector<int>> dp;
-//     int n;
-//     int lcs(string s, int i, int j) {
-//         if (i > j)
-//             return 0;
-//         if (i == j)
-//             return 1;
-//         if (dp[i][j] != -1)
-//             return dp[i][j];
-//         if (s[i] == s[j]) {
-//             return dp[i][j] =  2 + lcs(s, i + 1, j - 1);
-//         }
-//         if (s[i] != s[j]) {
-//             return dp[i][j] = max(lcs(s, i + 1, j), lcs(s, i, j - 1));
-//         }
-//         return dp[i][j];
-//     }
-//     int longestPalindromeSubseq(string s) {
-//         n = s.length();
-//         dp.assign(n + 1, vector<int>(n + 1, -1));
-//         return lcs(s, 0 , n-1);
-//     }
-// };
-
 class Solution {
 public:
-    int t[1001][1001];
-    int LPS(string& s, int i, int j) {
-        if(i > j)
+    vector<vector<int>> dp;
+    int n;
+    int lcs(string &s, int i, int j) {
+        if (i > j)
             return 0;
-        if(i == j)
+        if (i == j)
             return 1;
-        
-        if(t[i][j] != -1)
-            return t[i][j];
-        if(s[i] == s[j])
-            return t[i][j] = 2 + LPS(s, i+1, j-1);
-        else
-            return t[i][j] = max(LPS(s, i+1, j), LPS(s, i, j-1));
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (s[i] == s[j]) {
+            return dp[i][j] =  2 + lcs(s, i + 1, j - 1);
+        }
+        if (s[i] != s[j]) {
+            return dp[i][j] = max(lcs(s, i + 1, j), lcs(s, i, j - 1));
+        }
+        return dp[i][j];
     }
-    
     int longestPalindromeSubseq(string s) {
-        int m = s.length();
-        memset(t, -1, sizeof(t));
-        
-        return LPS(s, 0, m-1); //Approach-1
-        
+        n = s.length();
+        dp.assign(n, vector<int>(n, -1));
+        return lcs(s, 0 , n-1);
     }
 };
