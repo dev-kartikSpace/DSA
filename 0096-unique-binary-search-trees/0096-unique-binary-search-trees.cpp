cce567d9f6910147b32d105caf204ea71d2fde37
dev-kartikSpace <kartik200327@gmail.com>
2025-08-22 19:33:49 +0530

    Time: 0 ms (100%), Space: 8.4 MB (5.83%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     int numTrees(int n) {
-        vector<long long>dp(n+1 , 0);
-        dp[0]=1;
-        dp[1]=1;
-        for(int i = 2; i<=n; i++){
-            long long ways=0;
-            for(int j = 1; j<=i;j++){
-                ways += dp[j-1] * dp[i-j];
-            }
-            dp[i] = ways;
+        vector<long long>dp(n+1, -1);
+        return solve(n, dp);
+    }
+    long long solve(int n, vector<long long> &dp){
+        if(n<= 1) return 1;
+        if(dp[n] != -1) return dp[n];
+
+        long long ways = 0;
+        for(int i= 1; i<=n;i++){
+            ways += solve(i-1 , dp) * solve(n-i, dp);
         }
-        return dp[n];
+        dp[n] = ways;
+        return ways;
+
     }
 };
+
+
+// class Solution {
+// public:
+//     int numTrees(int n) {
+//         vector<long long>dp(n+1 , 0);
+//         dp[0]=1;
+//         dp[1]=1;
+//         for(int i = 2; i<=n; i++){
+//             long long ways=0;
+//             for(int j = 1; j<=i;j++){
+//                 ways += dp[j-1] * dp[i-j];
+//             }
+//             dp[i] = ways;
+//         }
+//         return dp[n];
+//     }
+// };
