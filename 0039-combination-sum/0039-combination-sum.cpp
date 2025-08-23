181cfc70d860d9f0470c364dac5e33b639276411
dev-kartikSpace <kartik200327@gmail.com>
2025-08-24 04:00:10 +0530

    Time: 0 ms (100%), Space: 14.1 MB (72.87%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    int n;
     void solve(vector<int>& candidates, int target, int start, vector<int>& path,
                vector<vector<int>>& result) {
         if (target == 0) {
@@ -10,14 +9,13 @@ public:
         if (target < 0)
             return;
 
-        for (int i = start; i < n; i++) {
+        for (int i = start; i <candidates.size() ; i++) {
             path.push_back(candidates[i]);
             solve(candidates, target - candidates[i], i, path, result);
             path.pop_back();
         }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        n = candidates.size();
         vector<vector<int>> result;
         vector<int> path;
         solve(candidates, target, 0, path, result);
