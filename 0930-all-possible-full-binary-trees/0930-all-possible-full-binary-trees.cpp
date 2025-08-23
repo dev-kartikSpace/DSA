db238189fc9512c160a4e9f1028d0cd5b09fcf58
dev-kartikSpace <kartik200327@gmail.com>
2025-08-23 19:07:27 +0530

    Time: 12 ms (38.73%), Space: 36.2 MB (21.35%) - LeetHub

--- 0930-all-possible-full-binary-trees/0930-all-possible-full-binary-trees.cpp
+++ 0930-all-possible-full-binary-trees/0930-all-possible-full-binary-trees.cpp
@@ -1,50 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<TreeNode*> allPossibleFBT(int n) {
-        // FBTs can only have an odd number of nodes
-        if (n % 2 == 0 || n == 0) {
+        if (n == 0 || n % 2 == 0)
             return {};
-        }
-        // Memoization cache to store results for each n
-        unordered_map<int, vector<TreeNode*>> memo;
-        return solve(n, memo);
+            
+        return solve(n);
     }
-    
-    vector<TreeNode*> solve(int n, unordered_map<int, vector<TreeNode*>>& memo) {
-        // If result for n is already computed, return it
-        if (memo.find(n) != memo.end()) {
-            return memo[n];
-        }
-        
+    vector<TreeNode*> solve(int n) {
         vector<TreeNode*> result;
-        // Base case: 1 node (leaf node)
         if (n == 1) {
             result.push_back(new TreeNode(0));
             return result;
         }
-        
-        // Try all possible ways to split n-1 nodes into left and right subtrees
-        for (int i = 1; i < n; i += 2) { // Left subtree must have odd nodes
+        for (int i = 1; i < n; i += 2) {
             int leftNodes = i;
-            int rightNodes = n - 1 - i; // Remaining nodes for right subtree
-            if (rightNodes < 0) continue; // Skip invalid cases
-            
-            // Recursively generate left and right subtrees
-            vector<TreeNode*> leftTrees = solve(leftNodes, memo);
-            vector<TreeNode*> rightTrees = solve(rightNodes, memo);
-            
-            // Combine each left and right subtree
+            int rightNodes = n - 1 - i;
+            vector<TreeNode*> leftTrees = solve(leftNodes);
+            vector<TreeNode*> rightTrees = solve(rightNodes);
+
             for (auto left : leftTrees) {
                 for (auto right : rightTrees) {
                     TreeNode* root = new TreeNode(0);
@@ -54,9 +27,6 @@ public:
                 }
             }
         }
-        
-        // Cache the result for n
-        memo[n] = result;
         return result;
     }
 };
