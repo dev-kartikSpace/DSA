1bc04414830de322ae710cdab319fd7b161df150
dev-kartikSpace <kartik200327@gmail.com>
2025-08-24 04:53:48 +0530

    Time: 0 ms (100%), Space: 10.5 MB (66.83%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-void solve(vector<int>& nums, int idx,vector<vector<int>> &result, vector<int> &path){
-    result.push_back(path);
-    for(int i = idx;i <nums.size(); i++){
-        if(i > idx && nums[i] == nums[i-1]) continue;
-        path.push_back(nums[i]);
-        solve(nums, i+1 , result, path);
-        path.pop_back();
+    void solve(vector<int>& nums, int idx, vector<vector<int>> &result, vector<int> &path) {
+        result.push_back(path);
+
+        for (int i = idx; i < nums.size(); i++) {
+            if (i > idx && nums[i] == nums[i-1]) continue; // skip duplicates
+            path.push_back(nums[i]);
+            solve(nums, i + 1, result, path);
+            path.pop_back();
+        }
     }
-}
+
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end()); 
         vector<vector<int>> result;
         vector<int> path;
         solve(nums, 0, result, path);
