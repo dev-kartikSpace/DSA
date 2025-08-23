df8527653a4fcd939fd61939560dbe1247866ab8
dev-kartikSpace <kartik200327@gmail.com>
2025-08-24 04:35:39 +0530

    Time: 2 ms (40.44%), Space: 9.9 MB (80.81%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
 void solve(vector<int>& nums, int idx,vector<vector<int>> &result, vector<int> &path){
     result.push_back(path);
-
     for(int i = idx;i <nums.size(); i++){
         path.push_back(nums[i]);
         solve(nums, i+1 , result, path);
