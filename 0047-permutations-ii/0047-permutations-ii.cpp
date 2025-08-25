4b57c494b9702fc66ecba558bba9887d70f7bac3
dev-kartikSpace <kartik200327@gmail.com>
2025-08-26 02:57:30 +0530

    Time: 3 ms (67.8%), Space: 11.4 MB (81.4%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+void solve(vector<int>& nums, vector<int> &path, vector<vector<int>> &result, vector<bool>& used){
+        if (path.size() == nums.size()) {
+            result.push_back(path);
+            return;
+        }
+
+    for(int i = 0; i<nums.size(); i++){
+        if(used[i]) continue; //skip the duplicates
+
+        if(i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
+
+        used[i] = true;
+        path.push_back(nums[i]);
+        solve(nums, path, result, used);
+        path.pop_back();
+        used[i] = false;
+    }
+}
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<int>path;
+        vector<vector<int>>result;
+        sort(nums.begin(), nums.end());
+        vector<bool> used(nums.size() , false);
+        solve(nums, path, result, used);
+        return result;
+    }
+};
