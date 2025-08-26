16c02b3ec58ea2401bc4b45d4afa440f2ea575aa
dev-kartikSpace <kartik200327@gmail.com>
2025-08-26 16:37:56 +0530

    Time: 1 ms (20.06%), Space: 13.8 MB (15.83%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -3,7 +3,7 @@ public:
     int compress(vector<char>& chars) {
         int ans = 0;
         for (int i = 0; i < chars.size();) {
-            const char letter = chars[i]; 
+            const char letter = chars[i];
             int count = 0;
             while (i < chars.size() && chars[i] == letter) {
                 ++count;
