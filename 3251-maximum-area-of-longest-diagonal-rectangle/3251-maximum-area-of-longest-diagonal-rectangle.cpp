958ee99c1e00c58d7ee7dac5b9275fcd22f48832
dev-kartikSpace <kartik200327@gmail.com>
2025-08-26 11:59:18 +0530

    Time: 0 ms (100%), Space: 29.4 MB (27.2%) - LeetHub

---
+++ 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
+        double maxRoot = 0; // Use double for precise diagonal comparison
+        int area = 0;
+        for (const auto & dim : dimensions) {
+            double sqroot = sqrt((long long)dim[0] * dim[0] + (long long)dim[1] * dim[1]);
+            int tempArea = dim[0] * dim[1];
+            if (maxRoot < sqroot || (abs(sqroot - maxRoot) < 1e-9 && tempArea > area)) {
+                area = tempArea;
+                maxRoot = sqroot;
+            }
+        }
+        return area;
+    }
+};
