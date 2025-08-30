8028d6f5fa72ba8f11c2a42de9aac3d69a40ace0
dev-kartikSpace <kartik200327@gmail.com>
2025-08-30 12:56:48 +0530

    Time: 0 ms (100%), Space: 16.1 MB (68.91%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -3,7 +3,6 @@ public:
     ListNode* deleteDuplicates(ListNode* head) {
         if (!head) return nullptr;
         ListNode* current = head;
-       
           while (current != nullptr && current->next != nullptr) {
             if (current->val == current->next->val) {
                 current->next = current->next->next;
