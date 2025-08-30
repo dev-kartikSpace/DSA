5405068aaf2e0e4c5b2c06b1a79d4cb1f93e3cfc
dev-kartikSpace <kartik200327@gmail.com>
2025-08-30 11:28:35 +0530

    Time: 4 ms (52.67%), Space: 25.9 MB (12.36%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,53 +1,18 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int n = 9; 
-        for (int i = 0; i < n; i++) {
-            unordered_map<char, int> count;
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] != '.') {
-                    if (count.find(board[i][j]) != count.end()) {
-                        return false; 
-                    }
-                    count[board[i][j]]++;
-                }
+        unordered_set<char> row[9], cols[9], boxes[9];
+        for(int i = 0; i<9; i++){
+            for(int j = 0;j<9; j++){
+                char c = board[i][j];
+                if(c=='.')continue;
+                int boxidx = (i/3)*3 + (j/3);
+                if(row[i].count(c) || cols[j].count(c) || boxes[boxidx].count(c)) return false;
+                row[i].insert(c);
+                cols[j].insert(c);
+                boxes[boxidx].insert(c);
             }
-        }
-
-        // Check columns
-        for (int j = 0; j < n; j++) {
-            unordered_map<char, int> count;
-            for (int i = 0; i < n; i++) {
-                if (board[i][j] != '.') {
-                    if (count.find(board[i][j]) != count.end()) {
-                        return false; 
-                    }
-                    count[board[i][j]]++;
-                }
-            }
-        }
-
-        // Check 3x3 sub-boxes
-        for (int boxRow = 0; boxRow < 3; boxRow++) {
-            for (int boxCol = 0; boxCol < 3; boxCol++) {
-                unordered_map<char, int> count;
-                for (int i = boxRow * 3; i < boxRow * 3 + 3; i++) {
-                    for (int j = boxCol * 3; j < boxCol * 3 + 3; j++) {
-                        if (board[i][j] != '.') {
-                            if (count.find(board[i][j]) != count.end()) {
-                                return false; 
-                            }
-                            count[board[i][j]]++;
-                        }
-                    }
-                }
-            }
-        }
-
-        return true; 
+        } 
+        return true;
     }
 };
