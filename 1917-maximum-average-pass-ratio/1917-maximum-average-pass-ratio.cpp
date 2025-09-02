9f719bda1686b8526bfbd42b86b83349df15d3e8
dev-kartikSpace <kartik200327@gmail.com>
2025-09-02 15:56:00 +0530

    Time: 377 ms (28.11%), Space: 99.1 MB (32.46%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
-        auto profit = [](int pass, int total) {
-            return (double)(pass + 1) / (total + 1) - (double)pass / total;
+        auto gain = [](int p, int q) {
+            return (double)(p + 1) / (q + 1) - (double)p / q;
         };
-        
-        priority_queue<pair<double, int>> pq;
-        for (int i = 0; i < classes.size(); i++) {
-            pq.push({profit(classes[i][0], classes[i][1]), i});
+
+        priority_queue<tuple<double,int,int>> pq;
+        for (auto &row : classes) {
+            int p = row[0], q = row[1];
+            pq.push({gain(p, q), p, q});
         }
-        
-        for (int i = 0; i < extraStudents; i++) {
-            auto [gain, idx] = pq.top();
+
+        while (extraStudents--) {
+            auto [g, p, q] = pq.top(); 
             pq.pop();
-            classes[idx][0]++;
-            classes[idx][1]++;
-            pq.push({profit(classes[idx][0], classes[idx][1]), idx});
+            p++; q++;
+            pq.push({gain(p, q), p, q});
         }
-        
+
         double sum = 0;
-        for (auto& c : classes) {
-            sum += (double)c[0] / c[1];
+        while (!pq.empty()) {
+            auto [g, p, q] = pq.top();
+            pq.pop();
+            sum += (double)p / q;
         }
-        
+
         return sum / classes.size();
     }
 };
-auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
