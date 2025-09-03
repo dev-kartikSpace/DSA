d2ee01ae9ac64e6cc774d37240a5470dddd2085d
dev-kartikSpace <kartik200327@gmail.com>
2025-09-04 04:07:08 +0530

    Time: 31 ms (34.01%), Space: 10.9 MB (58.69%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-int t[1001][1001];
-    int Check(const string& s, int i, int j) {
-        if(i>j) return true;
-        if(t[i][j] != -1) return t[i][j];
-        if(s[i] == s[j]) return t[i][j] = Check(s, i+1, j-1);
-        return t[i][j] = false;
-    }
     int countSubstrings(string s) {
         int n = s.length();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
         int count = 0;
-        memset(t, -1, sizeof(t));
-        for(int i=0; i<n; i++){
-            for(int j= i; j<n; j++){
-                if(Check(s, i, j)){
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (i == j) {
+                    dp[i][j] = true;
+                } else if (j == i + 1) {
+                    dp[i][j] = (s[i] == s[j]);
+                } else {
+                    dp[i][j] = (s[i] == s[j]) && dp[i + 1][j - 1];
+                }
+                if (dp[i][j]) {
                     count++;
                 }
             }
@@ -21,3 +21,27 @@ int t[1001][1001];
         return count;
     }
 };
+
+// class Solution {
+// public:
+// int t[1001][1001];
+//     int Check(const string& s, int i, int j) {
+//         if(i>j) return true;
+//         if(t[i][j] != -1) return t[i][j];
+//         if(s[i] == s[j]) return t[i][j] = Check(s, i+1, j-1);
+//         return t[i][j] = false;
+//     }
+//     int countSubstrings(string s) {
+//         int n = s.length();
+//         int count = 0;
+//         memset(t, -1, sizeof(t));
+//         for(int i=0; i<n; i++){
+//             for(int j= i; j<n; j++){
+//                 if(Check(s, i, j)){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
