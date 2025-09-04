4d7fcf62c4be5e49609af8584502eb416548edb0
dev-kartikSpace <kartik200327@gmail.com>
2025-09-04 13:56:49 +0530

    Time: 0 ms (100%), Space: 10.3 MB (91.93%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> dict(wordDict.begin(), wordDict.end());
-        int n = s.size();
-        vector<bool> dp(n + 1, false);
+        vector<bool> dp(s.size() + 1, false);
         dp[0] = true;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && dict.count(s.substr(j, i - j))) {
+        for (int i = 1; i <= s.size(); i++) {
+            for (const string& w : wordDict) {
+                int start = i - w.length();
+                if (start >= 0 && dp[start] && s.substr(start, w.length()) == w) {
                     dp[i] = true;
                     break;
                 }
             }
         }
-        return dp[n];
+        return dp[s.size()];        
     }
 };
+
+// class Solution {
+// public:
+//     bool wordBreak(string s, vector<string>& wordDict) {
+//         unordered_set<string> dict(wordDict.begin(), wordDict.end());
+//         int n = s.size();
+//         vector<bool> dp(n + 1, false);
+//         dp[0] = true;
+
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 0; j < i; j++) {
+//                 if (dp[j] && dict.count(s.substr(j, i - j))) {
+//                     dp[i] = true;
+//                     break;
+//                 }
+//             }
+//         }
+//         return dp[n];
+//     }
+// };
 
 // class Solution {
 // public:
