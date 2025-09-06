1f849bf6764d34ae0439f7ae0666a9e4db1ad8c8
dev-kartikSpace <kartik200327@gmail.com>
2025-09-06 11:11:02 +0530

    Time: 0 ms (100%), Space: 8.1 MB (100%) - LeetHub

--- 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.cpp
@@ -1,36 +1,52 @@
 class Solution {
 public:
     int makeTheIntegerZero(int num1, int num2) {
-        int t = 0;
-        while(true){
-            long long val = (long long) num1 - (long long)t * num2;
+        for (int t = 1; t <= 36; t++) {
+            long long val = (long long)num1 - (long long)t * num2;
 
-            if(val<0) return -1;
-            if(__builtin_popcountll(val) <= t && t<= val){
+            if (val < 0)
+                return -1;
+            if (__builtin_popcountll(val) <= t && t <= val) {
                 return t;
             }
-
-            t++;
         }
+        return -1;
     }
 };
 
-//class Solution {
+// class Solution {
 // public:
 //     int makeTheIntegerZero(int num1, int num2) {
-//         int temp = INT_MAX;
-//         int count = 0;
-//         for(int i=1; i<=60; i++){
-//             int remain = abs(num1 - (1 << i + num2));
-//             if(remain < temp && num1 !=0){
-//                 count++;
-//                 temp = min(temp, remain);
-//                 num1 = remain;
+//         int t = 0;
+//         while(true){
+//             long long val = (long long) num1 - (long long)t * num2;
+
+//             if(val<0) return -1;
+//             if(__builtin_popcountll(val) <= t && t<= val){
+//                 return t;
 //             }
-//             else if(num1 == 0){
-//                 return -1;
-//             }
+
+//             t++;
 //         }
-//         return true;
 //     }
 // };
+
+// class Solution {
+//  public:
+//      int makeTheIntegerZero(int num1, int num2) {
+//          int temp = INT_MAX;
+//          int count = 0;
+//          for(int i=1; i<=60; i++){
+//              int remain = abs(num1 - (1 << i + num2));
+//              if(remain < temp && num1 !=0){
+//                  count++;
+//                  temp = min(temp, remain);
+//                  num1 = remain;
+//              }
+//              else if(num1 == 0){
+//                  return -1;
+//              }
+//          }
+//          return true;
+//      }
+//  };
