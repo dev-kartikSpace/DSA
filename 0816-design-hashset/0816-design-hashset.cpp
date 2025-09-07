28050e65a350cfc67d234250c009cff39bac0bb1
dev-kartikSpace <kartik200327@gmail.com>
2025-09-07 15:54:21 +0530

    Time: 13 ms (67.83%), Space: 56.1 MB (27.17%) - LeetHub

--- 0816-design-hashset/0816-design-hashset.cpp
+++ 0816-design-hashset/0816-design-hashset.cpp
@@ -2,17 +2,18 @@ class MyHashSet {
 public:
     int n = 10000;
     vector<vector<int>> arr;
-    MyHashSet() { arr.resize(n); }
-
+    MyHashSet() {
+        arr.resize(n);  
+    }
+    
     void add(int key) {
         int idx = key % n;
         for (int x : arr[idx]) {
-            if (x == key)
-                return;
+            if (x == key) return;
         }
         arr[idx].push_back(key);
     }
-
+    
     void remove(int key) {
         int idx = key % n;
         for (int i = 0; i < arr[idx].size(); i++) {
@@ -22,21 +23,12 @@ public:
             }
         }
     }
-
+    
     bool contains(int key) {
         int idx = key % n;
         for (int x : arr[idx]) {
-            if (x == key)
-                return true;
+            if (x == key) return true;
         }
         return false;
     }
 };
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet* obj = new MyHashSet();
- * obj->add(key);
- * obj->remove(key);
- * bool param_3 = obj->contains(key);
- */
