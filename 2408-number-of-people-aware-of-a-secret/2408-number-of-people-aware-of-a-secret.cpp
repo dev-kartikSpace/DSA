989ddba4ea1d4ffe7d7d31a7ef50e85292f771e7
dev-kartikSpace <kartik200327@gmail.com>
2025-09-12 02:24:29 +0530

    Time: 16 ms (18.74%), Space: 9.5 MB (47.48%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -23,35 +23,3 @@ public:
         return result;
     }
 };
-
-//class Solution {
-    // public:
-    //     int M = 1e9 + 7;
-
-    //     int solve(int day, int delay, int forget, vector<int> &dp) {
-    //         if (day == 1) return 1;
-    //         if (dp[day] != -1) return dp[day];
-
-    //         long long result = 0;
-    //         for (int prev = day - forget + 1; prev <= day - delay;
-    //         prev++) {
-    //             if (prev > 0) {
-    //                 result = (result + solve(prev, delay, forget, dp)) %
-    //                 M;
-    //             }
-    //         }
-    //         return dp[day] = result;
-    //     }
-
-    //     int peopleAwareOfSecret(int n, int delay, int forget) {
-    //         long long total = 0;
-    //         vector<int> dp(n+1, -1);
-
-    //         for (int day = n - forget + 1; day <= n; day++) {
-    //             if (day > 0) {
-    //                 total = (total + solve(day, delay, forget, dp)) % M;
-    //             }
-    //         }
-    //         return total;
-    //     }
-    // };
