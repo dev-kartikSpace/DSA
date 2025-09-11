ce6e06c0a3be408df3f1b2450c9cc4c833b86093
dev-kartikSpace <kartik200327@gmail.com>
2025-09-12 02:28:18 +0530

    Time: 0 ms (100%), Space: 9.3 MB (71.26%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -4,22 +4,60 @@ public:
     int peopleAwareOfSecret(int n, int delay, int forget) {
         vector<long long> dp(n + 1);
         dp[1] = 1;
+        int count = 0;
         for (int day = 2; day <= n; day++) {
-            long long count = 0;
-            for (int prev = day - forget + 1; prev <= day - delay; prev++) {
-                if (prev > 0) {
-                    count = (count + dp[prev]) % M;
-                }
+            // for (int prev = day - forget + 1; prev <= day - delay; prev++) {
+            //     if (prev > 0) {
+            //         count = (count + dp[prev]) % M;
+            //     }
+            // }
+            if(day-delay> 0){
+                count = (count+dp[day-delay]) % M;
+            }
+            if(day-forget > 0){
+                count = (count - dp[day-forget])%M;
             }
             dp[day] = count;
         }
         int result = 0;
         for (int day = n - forget + 1; day <= n; day++) {
             if (day > 0) {
-                result = (result + dp[day]) % M;
+                result = (result + dp[day] + M) % M;
             }
         }
 
         return result;
     }
 };
+
+//class Solution {
+    // public:
+    //     int M = 1e9 + 7;
+
+    //     int solve(int day, int delay, int forget, vector<int> &dp) {
+    //         if (day == 1) return 1;
+    //         if (dp[day] != -1) return dp[day];
+
+    //         long long result = 0;
+    //         for (int prev = day - forget + 1; prev <= day - delay;
+    //         prev++) {
+    //             if (prev > 0) {
+    //                 result = (result + solve(prev, delay, forget, dp)) %
+    //                 M;
+    //             }
+    //         }
+    //         return dp[day] = result;
+    //     }
+
+    //     int peopleAwareOfSecret(int n, int delay, int forget) {
+    //         long long total = 0;
+    //         vector<int> dp(n+1, -1);
+
+    //         for (int day = n - forget + 1; day <= n; day++) {
+    //             if (day > 0) {
+    //                 total = (total + solve(day, delay, forget, dp)) % M;
+    //             }
+    //         }
+    //         return total;
+    //     }
+    // };
