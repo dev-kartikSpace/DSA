8f987097e61fb7ff5fe585c90653e30c2a1b2aba
dev-kartikSpace <kartik200327@gmail.com>
2025-09-12 02:13:36 +0530

    Time: 19 ms (15.7%), Space: 9.4 MB (47.48%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    int peopleAwareOfSecret(int n, int delay, int forget) {
-        vector<long long> dp(n + 1, 0);
-        dp[1] = 1;
-        long long share = 0, MOD = 1000000007;
-        for (int t = 2; t <= n; t++) {
-            if (t - delay > 0)
-                share = (share + dp[t - delay] + MOD) % MOD;
-            if (t - forget > 0)
-                share = (share - dp[t - forget] + MOD) % MOD;
-            dp[t] = share;
+    int M = 1e9 + 7;
+
+    int solve(int day, int delay, int forget, vector<int> &dp) {
+        if (day == 1) return 1;
+        if (dp[day] != -1) return dp[day];
+
+        long long result = 0;
+        for (int prev = day - forget + 1; prev <= day - delay; prev++) {
+            if (prev > 0) {
+                result = (result + solve(prev, delay, forget, dp)) % M;
+            }
         }
-        long long know = 0;
-        for (int i = n - forget + 1; i <= n; i++)
-            know = (know + dp[i]) % MOD;
-        return (int)know;
+        return dp[day] = result;
+    }
+
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        long long total = 0;
+        vector<int> dp(n+1, -1);
+
+        for (int day = n - forget + 1; day <= n; day++) {
+            if (day > 0) {
+                total = (total + solve(day, delay, forget, dp)) % M;
+            }
+        }
+        return total;
     }
 };
