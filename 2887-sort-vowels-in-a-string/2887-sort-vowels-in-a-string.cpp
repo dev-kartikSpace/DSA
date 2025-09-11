a42a72d2b11d85fe22dfe121a81e3789c3c604cc
dev-kartikSpace <kartik200327@gmail.com>
2025-09-11 12:04:57 +0530

    Time: 31 ms (9.75%), Space: 17.3 MB (20.99%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -1,55 +1,27 @@
-// class Solution {
-// public:
-//     string sortVowels(string s) {
-//         vector<char> vowels;
-//         string VowelSet = "aeiouAEIOU";
-//         for(int i=0; i<s.length(); i++){
-//             char c = s[i];
-//             //npos => return found and not found
-//             if(VowelSet.find(c) != string::npos){
-//                 vowels.push_back(c);
-//             }
-//         }
-//         sort(vowels.begin(), vowels.end());
-//         int j= 0;
-//         string ans;
-//         for(int i=0;i<s.length(); i++){
-//             if (VowelSet.find(s[i]) != string::npos){
-//                 ans += vowels[j];
-//                 j++;
-//             }
-//             else{
-//                 ans += s[i];
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     string sortVowels(string s) {
         vector<char> vowels;
-        for (char c : s) {
-            if (isVowel(c)) {
+        string VowelSet = "aeiouAEIOU";
+        for(int i=0; i<s.length(); i++){
+            char c = s[i];
+            //npos => return found and not found
+            if(VowelSet.find(c) != string::npos){
                 vowels.push_back(c);
             }
         }
-
         sort(vowels.begin(), vowels.end());
-
-        int v_index = 0;
-        for (int i = 0; i < s.size(); i++) {
-            if (isVowel(s[i])) {
-                s[i] = vowels[v_index++];
+        int j= 0;
+        string ans;
+        for(int i=0;i<s.length(); i++){
+            if (VowelSet.find(s[i]) != string::npos){
+                ans += vowels[j];
+                j++;
+            }
+            else{
+                ans += s[i];
             }
         }
-
-        return s;
-    }
-
-private:
-    bool isVowel(char c) {
-        return string("AEIOUaeiou").find(c) != string::npos;
+        return ans;
     }
 };
