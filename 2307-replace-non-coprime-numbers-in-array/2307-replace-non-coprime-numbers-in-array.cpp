2bef669400a21da08d312ade817f2a84ddd46e01
dev-kartikSpace <kartik200327@gmail.com>
2025-09-16 11:39:34 +0530

    Time: 27 ms (39.35%), Space: 133.2 MB (18.52%) - LeetHub

--- 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.cpp
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.cpp
@@ -1,22 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-
     vector<int> replaceNonCoprimes(vector<int>& nums) {
-        vector<int> st;
-        for(int num: nums){
+        vector<long long> st; // use long long for safety
+        
+        for (int num : nums) {
             st.push_back(num);
-            while(st.size() > 1){
-                int a = st.back();
-                int b = st[st.size() - 2];
-
-                int g = gcd(a,b);
-                if(g == 1) break;
-
-                long long lcm = a/g * b;
+            while (st.size() > 1) {
+                long long a = st.back();
+                long long b = st[st.size() - 2];
+                long long g = gcd(a, b);
+                
+                if (g == 1) break;
+                
+                long long lcm = a / g * b; 
                 st.pop_back();
                 st.back() = lcm;
             }
         }
-        return st;
+    
+        vector<int> ans(st.begin(), st.end());
+        return ans;
     }
 };
