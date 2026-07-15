b21da76d9aae26da6f556be45829943c4639db62
dev-kartikSpace <kartik200327@gmail.com>
2026-07-15 21:33:18 +0530

    Time: 2 ms (7.52%), Space: 9.4 MB (41.95%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> ans(n, vector<int>(n));
-
-        int top = 0;
-        int bottom = n - 1;
+        int right  = n-1;
         int left = 0;
-        int right = n - 1;
+        int bottom = n-1;
+        int top = 0;
 
         int count = 1;
+        vector<vector<int>> ans(n, vector<int>(n,0));
+        while(top <= bottom && left <= right){
 
-        while (top <= bottom && left <= right) {
-
-            for (int j = left; j <= right; j++) {
-                ans[top][j] = count++;
+            for(int j = left; j <= right; j++){
+                ans[top][j] = count;
+                count++;
             }
-            top++;
+            top ++;
 
-            for (int i = top; i <= bottom; i++) {
-                ans[i][right] = count++;
+            for(int i = top; i <= bottom; i++){
+                ans[i][right] = count;
+                count++;
             }
             right--;
 
-            if (top <= bottom) {
-                for (int j = right; j >= left; j--) {
-                    ans[bottom][j] = count++;
+            if(top <= bottom){
+                for(int j = right; j >= left; j--){
+                    ans[bottom][j] = count;
+                    count++;
                 }
-                bottom--;
             }
 
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    ans[i][left] = count++;
-                }
-                left++;
+            bottom --;
+
+            for(int i = bottom; i >= top; i--){
+                ans[i][left] = count;
+                count ++;
             }
+            left ++;
         }
-
         return ans;
     }
 };
