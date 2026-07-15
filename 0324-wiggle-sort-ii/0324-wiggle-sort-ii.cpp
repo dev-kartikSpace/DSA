c5c678f648c35b197c94e18d098e051be91d7f16
dev-kartikSpace <kartik200327@gmail.com>
2026-07-15 10:11:44 +0530

    Time: 7 ms (13.01%), Space: 21.9 MB (46.54%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -1,30 +1,20 @@
-// class Solution {
-// public:
-//     void wiggleSort(vector<int>& nums) {
-//         int n = nums.size();
-//         for(int i= 1 ; i<n; i++){
-//             if((i % 2 != 0 && nums[i-1] > nums[i]) || (i % 2 == 0 &&  nums[i-1] < nums[i])){
-//                 swap(nums[i-1], nums[i]);
-//             }
-//         }
-//     }
-// };
-
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        vector<int> sorted = nums;
-        sort(sorted.begin(), sorted.end());
-        
         int n = nums.size();
-        int mid = (n - 1) / 2;   // end of smaller half
-        
-        // Place smaller numbers on even indices, larger on odd
-        for (int i = 0; i < n; i += 2) {
-            nums[i] = sorted[mid - i/2];
-        }
-        for (int i = 1; i < n; i += 2) {
-            nums[i] = sorted[n - 1 - i/2];
+        vector<int> temp = nums;
+        sort(temp.begin(), temp.end());
+
+        int small = (n-1)/2;
+        int large = n-1;
+
+        for(int i=0; i<n; i++){
+            if(i%2 == 0){
+                nums[i] = temp[small--];
+            }
+            else{
+                nums[i] = temp[large--];
+            }
         }
     }
 };
