5f370abf991fce572403a3c6c6567e36fe8a652f
kartik sharma <kartik200327@gmail.com>
2026-08-18 18:59:27 +0530

    Time: 0 ms (100%), Space: 60.9 MB (52.67%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -22,7 +22,7 @@ void inorder(TreeNode* root){
     inorder(root->left);
 
     if(prev != NULL && prev -> val > root->val){
-        if(!first) first = prev;
+        if(first == nullptr) first = prev;
         second = root;
     }
     prev = root;
